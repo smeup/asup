0164b599903224227f088d496c36068c719aa043
Mattia Rocchi <mattiarocchi@gmail.com>
2015-06-18 14:38:13 +0200

    Release 0.7.0

--- org.smeup.sys.ui.pdme.e4/src/org/smeup/sys/ui/pdme/e4/actions/NormalizeCommandsAction.java
+++ org.smeup.sys.ui.pdme.e4/src/org/smeup/sys/ui/pdme/e4/actions/NormalizeCommandsAction.java
@@ -29,10 +29,10 @@ import org.smeup.sys.os.cmd.QCommandParameter;
 public class NormalizeCommandsAction implements IObjectActionDelegate {
 
 	private Shell shell;
-	
+
 	private Resource resource;
 	private QCommandContainer commandContainer;
-	
+
 	/**
 	 * Constructor for Action1.
 	 */
@@ -52,14 +52,14 @@ public class NormalizeCommandsAction implements IObjectActionDelegate {
 	 */
 	public void run(IAction action) {
 
-		for(QCommand command: commandContainer.getContents()) {
+		for (QCommand command : commandContainer.getContents()) {
 			System.out.println(command.getName());
-			
-			for(QCommandParameter commandParameter: command.getParameters()) {
+
+			for (QCommandParameter commandParameter : command.getParameters()) {
 				commandParameter.getDataTerm().accept(new CommandsVisitor());
 			}
 		}
-		
+
 		try {
 			this.resource.save(Collections.EMPTY_MAP);
 		} catch (IOException e) {
@@ -71,87 +71,86 @@ public class NormalizeCommandsAction implements IObjectActionDelegate {
 	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
 	 */
 	public void selectionChanged(IAction action, ISelection selection) {
-		action.setEnabled(false);		
-		
+		action.setEnabled(false);
+
 		this.commandContainer = null;
 		this.resource = null;
-		
-		if(selection instanceof TreeSelection) {
+
+		if (selection instanceof TreeSelection) {
 			TreeSelection treeSelection = (TreeSelection) selection;
 
 			Object object = treeSelection.getFirstElement();
-			if(object instanceof IFile) {
-				try {	
-					this.commandContainer = loadEcore((IFile)object);
+			if (object instanceof IFile) {
+				try {
+					this.commandContainer = loadEcore((IFile) object);
 				} catch (Exception e) {
 					MessageDialog.openError(shell, "Load commands file", e.getMessage());
 				}
-			}	
+			}
 		}
-		
+
 		action.setEnabled(true);
-		
+
 	}
-		
+
 	private QCommandContainer loadEcore(IFile commandsFile) throws IOException {
-		
-		ResourceSet resourceSet = new ResourceSetImpl(); 
-		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl()); 			 
-	    URI uri = URI.createURI(commandsFile.getLocationURI().toString());
+
+		ResourceSet resourceSet = new ResourceSetImpl();
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+		URI uri = URI.createURI(commandsFile.getLocationURI().toString());
 		Resource resource = resourceSet.getResource(uri, true);
 		resource.load(Collections.EMPTY_MAP);
-		
+
 		QCommandContainer commandContainer = (QCommandContainer) resource.getContents().get(0);
-		
+
 		this.resource = resource;
-		
+
 		return commandContainer;
 	}
-	
+
 	private class CommandsVisitor extends DataTermVisitorImpl {
 
 		@Override
 		public boolean visit(QDataTerm<?> term) {
+
+			System.out.println(term);
 			
 			QCardinality cardinality = term.getCardinality();
-			if(cardinality != null)
-				return true;
-			
-			cardinality = term.getFacet(QCardinality.class);
-			if(cardinality != null) {
-				term.setCardinality(cardinality);
-				term.getFacets().remove(cardinality);
-				return true;
+			if (cardinality == null) {
+
+				cardinality = term.getFacet(QCardinality.class);
+				if (cardinality != null) {
+					term.setCardinality(cardinality);
+					term.getFacets().remove(cardinality);
+				} else {
+					if (term.isMandatory()) {
+
+						cardinality = QIntegratedLanguageCoreMetaFactory.eINSTANCE.createCardinality();
+						term.setCardinality(cardinality);
+
+						if (cardinality.getMin() == 0)
+							cardinality.setMin(1);
+
+						term.setMandatory(false);
+					}
+				}
 			}
 			
-			if(term.isMandatory()) {
-				System.out.println("\t"+term);
-				
-				cardinality = QIntegratedLanguageCoreMetaFactory.eINSTANCE.createCardinality();
-				term.setCardinality(cardinality);
-				
-				if(cardinality.getMin()==0)
-					cardinality.setMin(1);
-				
-				term.setMandatory(false);
-			}
-			
-			if(term.getDefault() != null) {
-				System.out.println("\t"+term);
-				
+			if (term.getDefault() != null) {
+
 				QDefault _default = term.getFacet(QDefault.class);
-				if(_default == null) {
+				if (_default == null) {
 					_default = QIntegratedLanguageCoreMetaFactory.eINSTANCE.createDefault();
 					term.getFacets().add(_default);
 				}
-				
-				if(_default.getValue() == null) 
+
+				if (_default.getValue() == null)
 					_default.setValue(term.getDefault());
 
 				term.setDefault(null);
 			}
-			
+
 			return super.visit(term);
-		}		
+		}
 	}
 }
