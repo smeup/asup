1e4fe0b5294087d30843fd87b99bd48b81763fb0
Franco Lombardo <f.lombardo69@gmail.com>
2015-08-04 17:00:04 +0200

    Resource leaks - Part 2

--- org.smeup.sys.os.type.base/src/org/smeup/sys/os/type/base/api/ObjectWorker.java
+++ org.smeup.sys.os.type.base/src/org/smeup/sys/os/type/base/api/ObjectWorker.java
@@ -16,7 +16,6 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import org.smeup.sys.dk.core.annotation.ToDo;
 import org.smeup.sys.il.core.QObject;
 import org.smeup.sys.il.core.QObjectIterator;
 import org.smeup.sys.il.core.out.QObjectWriter;
@@ -35,6 +34,7 @@ import org.smeup.sys.os.core.resources.QResourceReader;
 import org.smeup.sys.os.type.QType;
 import org.smeup.sys.os.type.QTypeRegistry;
 import org.smeup.sys.os.type.QTypedObject;
+import org.smeup.sys.os.type.base.api.ObjectWorker.Object.NameGenericEnum;
 
 @Program(name = "QMNWRKXX")
 public class ObjectWorker {
@@ -50,82 +50,93 @@ public class ObjectWorker {
 	@Inject
 	private QJobLogManager jobLogManager;
 
-	public @Entry void main(@ToDo @DataDef(qualified = true) Object object, @ToDo @DataDef(length = 7) QCharacter objectType, @ToDo @DataDef(length = 50) QCharacter text,
-			@ToDo @DataDef(length = 2) QCharacter application) {
+	public @Entry void main(@DataDef(qualified = true) Object object, 
+							@DataDef(length = 7) QCharacter objectType,
+							@DataDef(length = 50) QCharacter text,
+							@DataDef(length = 2) QCharacter application) {
 
+		QObjectWriter objectWriter = outputManager.getDefaultWriter(job.getContext());
+		objectWriter.initialize();
+
+		List<QType<?>> types = findTypes(objectType);
+		for (QType<?> type : types) {
+			QResourceReader<?> resourceReader = findReader(object, type);
+			try(QObjectIterator<?> objectIterator = findIterator(object.nameGeneric, resourceReader);) {
+				while (objectIterator.hasNext()) {
+					QObject qObject = null;
+					try {
+						qObject = objectIterator.next();
+	
+						if (qObject instanceof QTypedObject) {
+							QTypedObject qTypedObject = (QTypedObject) qObject;
+							// text
+							if (!text.isNull() && !text.isEmpty())
+								if (qTypedObject.getText() == null || qTypedObject.getText().toUpperCase().indexOf(text.trimR().toUpperCase()) < 0)
+									continue;
+	
+							// application
+							if (!application.isNull() && !application.isEmpty())
+								if (qTypedObject.getApplication() == null || qTypedObject.getApplication().toUpperCase().indexOf(application.trimR().toUpperCase()) < 0)
+									continue;
+	
+						}
+						objectWriter.write(qObject);
+					} catch (Exception e) {
+						jobLogManager.error(job, qObject + " " + e.getMessage());
+					}
+				}
+			}
+		}
+		
+		objectWriter.flush();
+	}
+
+	private List<QType<?>> findTypes(QCharacter objectType) {
 		List<QType<?>> types = new ArrayList<QType<?>>();
 		if (objectType.trimR().equals("*ALL"))
 			types.addAll(typeRegistry.list());
 		else
 			types.add(typeRegistry.lookup(objectType.trimR()));
+		return types;
+	}
 
-		QObjectWriter objectWriter = outputManager.getDefaultWriter(job.getContext());
-		objectWriter.initialize();
-		for (QType<?> type : types) {
-
-			// System.out.println(qType);
-
-			QResourceReader<?> resourceReader = null;
-			switch (object.library.asEnum()) {
-			case ALL:
-				resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), Scope.ALL);
-				break;
-			case ALLUSR:
-				resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), Scope.ALL_USER);
-				break;
-			case CURLIB:
-				resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), Scope.CURRENT_LIBRARY);
-				break;
-			case LIBL:
-				resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), Scope.LIBRARY_LIST);
-				break;
-			case USRLIBL:
-				resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), Scope.USER_LIBRARY_LIST);
-				break;
-			case OTHER:
-				resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), object.library.asData().trimR());
-				break;
-			}
-
-			QObjectIterator<?> objectIterator = null;
-
-			switch (object.nameGeneric.asEnum()) {
-			case ALL:
-				objectIterator = resourceReader.find(null);
-				break;
-
-			case OTHER:
-				objectIterator = resourceReader.find(object.nameGeneric.asData().trimR());
-				break;
-
-			}
-
-			QObject qObject = null;
-			while (objectIterator.hasNext())
-				try {
-					qObject = objectIterator.next();
-
-					if (qObject instanceof QTypedObject) {
-						QTypedObject qTypedObject = (QTypedObject) qObject;
-						// text
-						if (!text.isNull() && !text.isEmpty())
-							if (qTypedObject.getText() == null || qTypedObject.getText().toUpperCase().indexOf(text.trimR().toUpperCase()) < 0)
-								continue;
-
-						// application
-						if (!application.isNull() && !application.isEmpty())
-							if (qTypedObject.getApplication() == null || qTypedObject.getApplication().toUpperCase().indexOf(application.trimR().toUpperCase()) < 0)
-								continue;
-
-					}
-					objectWriter.write(qObject);
-
-				} catch (Exception e) {
-					jobLogManager.error(job, qObject + " " + e.getMessage());
-				}
-			objectIterator.close();
+	private QResourceReader<?> findReader(Object object, QType<?> type) {
+		QResourceReader<?> resourceReader = null;
+		switch (object.library.asEnum()) {
+		case ALL:
+			resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), Scope.ALL);
+			break;
+		case ALLUSR:
+			resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), Scope.ALL_USER);
+			break;
+		case CURLIB:
+			resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), Scope.CURRENT_LIBRARY);
+			break;
+		case LIBL:
+			resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), Scope.LIBRARY_LIST);
+			break;
+		case USRLIBL:
+			resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), Scope.USER_LIBRARY_LIST);
+			break;
+		case OTHER:
+			resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), object.library.asData().trimR());
+			break;
 		}
-		objectWriter.flush();
+		return resourceReader;
+	}
+
+	@SuppressWarnings("resource")
+	private QObjectIterator<?> findIterator(QEnum<NameGenericEnum, QCharacter> nameGeneric,	QResourceReader<?> resourceReader) {
+		QObjectIterator<?> result = null;
+		switch (nameGeneric.asEnum()) {
+		case ALL:
+			result = resourceReader.find(null);
+			break;
+		case OTHER:
+			result = resourceReader.find(nameGeneric.asData().trimR());
+			break;
+		}
+		return result;
 	}
 
 	public static class Object extends QDataStructWrapper {
