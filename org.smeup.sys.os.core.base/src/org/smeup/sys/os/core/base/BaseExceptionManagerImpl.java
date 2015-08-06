a48a8de2e7ca4486642109bf28ef5d22d1199bc2
Mattia Rocchi <mattiarocchi@gmail.com>
2015-08-06 10:04:16 +0200

    QCPFMSG chached

--- org.smeup.sys.os.core.base/src/org/smeup/sys/os/core/base/BaseExceptionManagerImpl.java
+++ org.smeup.sys.os.core.base/src/org/smeup/sys/os/core/base/BaseExceptionManagerImpl.java
@@ -16,18 +16,35 @@ import org.smeup.sys.os.msgf.QMessageFile;
 
 public class BaseExceptionManagerImpl implements QExceptionManager {
 
-
 	@Inject
 	private QResourceManager resourceManager;
 	@Inject
 	private QJobLogManager jobLogManager;
-	
+
+	private QMessageFile cpfMessageFile;
+
 	@Override
 	public <E extends Enum<E>> OperatingSystemMessageException prepareException(QJob job, Enum<E> message, Object[] variables) {
 
 		QResourceReader<QMessageFile> messageFileReader = resourceManager.getResourceReader(job, QMessageFile.class, Scope.LIBRARY_LIST);
-		
-		QMessageFile qMessageFile = messageFileReader.lookup(message.getClass().getSimpleName());
+
+		QMessageFile qMessageFile = null;
+
+		if (message.getClass().getSimpleName().equalsIgnoreCase("QCPFMSG")) {
+			if (cpfMessageFile == null) {
+				synchronized (this) {
+					if (cpfMessageFile == null) {
+						cpfMessageFile = messageFileReader.lookup(message.getClass().getSimpleName());
+						qMessageFile = cpfMessageFile;
+					}
+				}
+				qMessageFile = cpfMessageFile;
+			}
+			else
+				qMessageFile = cpfMessageFile;
+
+		} else
+			qMessageFile = messageFileReader.lookup(message.getClass().getSimpleName());
 
 		QMessageDescription messageDescription = null;
 		if (qMessageFile != null)
@@ -50,7 +67,7 @@ public class BaseExceptionManagerImpl implements QExceptionManager {
 		OperatingSystemMessageException messageException = new OperatingSystemMessageException(name, messageText, severity);
 
 		jobLogManager.addEntry(job, severity, messageText);
-		
+
 		return messageException;
 
 	}
