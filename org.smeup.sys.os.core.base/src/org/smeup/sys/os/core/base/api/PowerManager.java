fc54811b49e1af08a246e244ab83e33e41643fca
Franco Lombardo <f.lombardo69@gmail.com>
2015-08-06 18:11:12 +0200

    PWRDWNSYS - 1

---
+++ org.smeup.sys.os.core.base/src/org/smeup/sys/os/core/base/api/PowerManager.java
@@ -0,0 +1,27 @@
+package org.smeup.sys.os.core.base.api;
+
+import javax.inject.Inject;
+
+import org.smeup.sys.dk.core.annotation.Supported;
+import org.smeup.sys.il.data.QCharacter;
+import org.smeup.sys.il.data.QEnum;
+import org.smeup.sys.il.data.annotation.DataDef;
+import org.smeup.sys.il.data.annotation.Entry;
+import org.smeup.sys.il.data.annotation.Program;
+import org.smeup.sys.rt.core.QApplicationManager;
+
+public  @Program(name = "QWCCSDSC")  class PowerManager {
+
+	@Inject
+	private QApplicationManager applicationManager;
+	
+	public @Entry void main(@Supported @DataDef(length = 4) QEnum<RESTARTEnum, QCharacter> restart) {
+		//TODO
+		applicationManager.restart();
+	}
+	
+	public static enum RESTARTEnum {
+		NO, 
+		YES
+	}
+}
