8ef815ed9c148319a9610c085ac8fea9610654b4
Franco Lombardo <f.lombardo69@gmail.com>
2015-09-28 12:36:53 +0200

    Added Copyright Note

--- org.smeup.sys.os.dtaara.base/src/org/smeup/sys/os/dtaara/base/api/DataAreaRetreiver.java
+++ org.smeup.sys.os.dtaara.base/src/org/smeup/sys/os/dtaara/base/api/DataAreaRetreiver.java
@@ -1,12 +1,21 @@
+/**
+ *  Copyright (c) 2012, 2015 Sme.UP and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *   Franco Lombardo - Initial API and implementation
+ */
 package org.smeup.sys.os.dtaara.base.api;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.smeup.sys.dk.core.annotation.ToDo;
-import org.smeup.sys.il.core.java.QStrings;
 import org.smeup.sys.il.data.QAdapter;
-import org.smeup.sys.il.data.QBufferedData;
 import org.smeup.sys.il.data.QCharacter;
 import org.smeup.sys.il.data.QDataFactory;
 import org.smeup.sys.il.data.QDataManager;
@@ -17,7 +26,6 @@ import org.smeup.sys.os.core.QExceptionManager;
 import org.smeup.sys.os.core.jobs.QJob;
 import org.smeup.sys.os.dtaara.QDataArea;
 import org.smeup.sys.os.dtaara.QDataAreaManager;
-import org.smeup.sys.os.dtaara.base.api.DataAreaModifier.QCPFMSG;
 import org.smeup.sys.os.dtaara.base.api.tools.DataAreaSpecification;
 import org.smeup.sys.os.dtaara.base.api.tools.DataAreaSpecification.SUBSTRINGSPECIFICATIONS;
 import org.smeup.sys.os.dtaara.base.api.tools.ExistingDataAreaSpecification.DataAreaNotFoundException;
