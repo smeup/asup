a3eee1c1c601c642b32fafdbed4a42d9c419c75f
Franco Lombardo <f.lombardo69@gmail.com>
2015-09-28 18:03:11 +0200

    RTJJOBA - primissima bozza

--- org.smeup.sys.os.core.base/src/org/smeup/sys/os/core/base/api/JobAttributesRetriever.java
+++ org.smeup.sys.os.core.base/src/org/smeup/sys/os/core/base/api/JobAttributesRetriever.java
@@ -32,39 +32,84 @@ public class JobAttributesRetriever {
 	@Inject
 	private QJobManager jobManager;
 
-	public @Entry void main(@ToDo @DataDef(length = 10) QCharacter cLVarForJOB10, @ToDo @DataDef(length = 10) QCharacter cLVarForUSER10, @ToDo @DataDef(length = 6) QCharacter cLVarForNBR6,
-			@ToDo @DataDef(length = 10) QCharacter cLVarForCURUSER10, @ToDo @DataDef(length = 1) QCharacter cLVarForTYPE1, @ToDo @DataDef(length = 1) QCharacter cLVarForSUBTYPE1,
-			@ToDo @DataDef(length = 165) QCharacter cLVarForSYSLIBL165, @ToDo @DataDef(length = 10) QCharacter cLVarForCURLIB10, @ToDo @DataDef(length = 275) QCharacter userLibraries,
-			@ToDo @DataDef(length = 10) QCharacter cLVarForASPGRP10, @ToDo @DataDef(length = 1) QCharacter cLVarForLOGLVL1, @ToDo @DataDef(precision = 2) QDecimal cLVarForLOGSEV20,
-			@ToDo @DataDef(length = 10) QCharacter cLVarForLOGTYPE10, @ToDo @DataDef(length = 10) QCharacter cLVarForLOGCLPGM10, @ToDo @DataDef(length = 10) QCharacter cLVarForLOGOUTPUT10,
-			@ToDo @DataDef(precision = 2) QDecimal cLVarForJOBMSGQMX20, @ToDo @DataDef(length = 10) QCharacter cLVarForJOBMSGQFL10, @ToDo @DataDef(length = 10) QCharacter cLVarForINQMSGRPY10,
-			@ToDo @DataDef(length = 7) QCharacter cLVarForSTSMSG7, @ToDo @DataDef(length = 7) QCharacter cLVarForBRKMSG7, @ToDo @DataDef(length = 13) QCharacter cLVarForDEVRCYACN13,
-			@ToDo @DataDef(precision = 5) QDecimal cLVarForRTNCDE50, @ToDo @DataDef(length = 1) QCharacter cLVarForENDSTS1, @ToDo @DataDef(length = 10) QCharacter cLVarForPRTDEV10,
-			@ToDo @DataDef(length = 10) QCharacter cLVarForOUTQ10, @ToDo @DataDef(length = 10) QCharacter cLVarForOUTQLIB10, @ToDo @DataDef(length = 10) QCharacter cLVarForSPLFACN10,
-			@ToDo @DataDef(length = 30) QCharacter cLVarForPRTTXT30, @ToDo @DataDef(length = 10) QCharacter cLVarForPRTKEYFMT10, @ToDo @DataDef(length = 10) QCharacter cLVarForSRTSEQ10,
-			@ToDo @DataDef(length = 10) QCharacter cLVarForSRTSEQLIB10, @ToDo @DataDef(length = 3) QCharacter cLVarForLANGID3, @ToDo @DataDef(length = 2) QCharacter cLVarForCNTRYID2,
-			@ToDo @DataDef(precision = 5) QDecimal cLVarForCCSID50, @ToDo @DataDef(precision = 5) QDecimal cLVarForDFTCCSID50, @ToDo @DataDef(length = 10) QCharacter cLVarForCHRIDCTL10,
-			@ToDo @DataDef(length = 1) QCharacter cLVarForDECFMT1, @ToDo @DataDef(length = 4) QCharacter cLVarForDATFMT4, @ToDo @DataDef(length = 1) QCharacter cLVarForDATSEP1,
-			@ToDo @DataDef(length = 1) QCharacter cLVarForTIMSEP1, @ToDo @DataDef(length = 6) QCharacter cLVarForDATE6, @ToDo @DataDef(length = 7) QCharacter cLVarForCYMDDATE7,
-			@ToDo @DataDef(length = 20) QCharacter cLVarForDATETIME20, @ToDo @DataDef(length = 4) QCharacter cLVarForDAYOFWEEK4, @ToDo @DataDef(length = 10) QCharacter cLVarForTIMZON10,
-			@ToDo @DataDef(length = 10) QCharacter cLVarForTIMZONABBR10, @ToDo @DataDef(length = 50) QCharacter cLVarForTIMZONFULL50, @ToDo @DataDef(precision = 3) QDecimal cLVarForTIMOFFSET30,
-			@ToDo @DataDef(precision = 3) QDecimal cLVarForYEAROFS30, @ToDo @DataDef(length = 15) QCharacter cLVarForACGCDE15, @ToDo @DataDef(length = 8) QCharacter cLVarForSWS8,
-			@ToDo @DataDef(precision = 2) QDecimal cLVarForRUNPTY20, @ToDo @DataDef(precision = 7) QDecimal cLVarForTIMESLICE70, @ToDo @DataDef(length = 10) QCharacter cLVarForPURGE10,
-			@ToDo @DataDef(precision = 7) QDecimal cLVarForDFTWAIT70, @ToDo @DataDef(length = 10) QCharacter cLVarForSBMMSGQ10, @ToDo @DataDef(length = 10) QCharacter cLVarForSBMMSGQLIB10,
-			@ToDo @DataDef(length = 5) QCharacter cLVarForDDMCNV5, @ToDo @DataDef(length = 10) QCharacter cLVarForTSEPOOL10, @ToDo @DataDef(length = 20) QCharacter cLVarForTHDRSCAFN20,
+	public @Entry void main(
+			@ToDo @DataDef(length = 10) QCharacter cLVarForJOB10,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForUSER10,
+			@ToDo @DataDef(length = 6) QCharacter cLVarForNBR6,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForCURUSER10,
+			@ToDo @DataDef(length = 1) QCharacter cLVarForTYPE1,
+			@ToDo @DataDef(length = 1) QCharacter cLVarForSUBTYPE1,
+			@DataDef(length = 165) QCharacter sysLibraries,
+			@DataDef(length = 10) QCharacter curLibrary,
+			@DataDef(length = 275) QCharacter userLibraries,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForASPGRP10,
+			@ToDo @DataDef(length = 1) QCharacter cLVarForLOGLVL1,
+			@ToDo @DataDef(precision = 2) QDecimal cLVarForLOGSEV20,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForLOGTYPE10,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForLOGCLPGM10,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForLOGOUTPUT10,
+			@ToDo @DataDef(precision = 2) QDecimal cLVarForJOBMSGQMX20,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForJOBMSGQFL10,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForINQMSGRPY10,
+			@ToDo @DataDef(length = 7) QCharacter cLVarForSTSMSG7,
+			@ToDo @DataDef(length = 7) QCharacter cLVarForBRKMSG7,
+			@ToDo @DataDef(length = 13) QCharacter cLVarForDEVRCYACN13,
+			@ToDo @DataDef(precision = 5) QDecimal cLVarForRTNCDE50,
+			@ToDo @DataDef(length = 1) QCharacter cLVarForENDSTS1,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForPRTDEV10,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForOUTQ10,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForOUTQLIB10,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForSPLFACN10,
+			@ToDo @DataDef(length = 30) QCharacter cLVarForPRTTXT30,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForPRTKEYFMT10,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForSRTSEQ10,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForSRTSEQLIB10,
+			@ToDo @DataDef(length = 3) QCharacter cLVarForLANGID3,
+			@ToDo @DataDef(length = 2) QCharacter cLVarForCNTRYID2,
+			@ToDo @DataDef(precision = 5) QDecimal cLVarForCCSID50,
+			@ToDo @DataDef(precision = 5) QDecimal cLVarForDFTCCSID50,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForCHRIDCTL10,
+			@ToDo @DataDef(length = 1) QCharacter cLVarForDECFMT1,
+			@ToDo @DataDef(length = 4) QCharacter cLVarForDATFMT4,
+			@ToDo @DataDef(length = 1) QCharacter cLVarForDATSEP1,
+			@ToDo @DataDef(length = 1) QCharacter cLVarForTIMSEP1,
+			@ToDo @DataDef(length = 6) QCharacter cLVarForDATE6,
+			@ToDo @DataDef(length = 7) QCharacter cLVarForCYMDDATE7,
+			@ToDo @DataDef(length = 20) QCharacter cLVarForDATETIME20,
+			@ToDo @DataDef(length = 4) QCharacter cLVarForDAYOFWEEK4,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForTIMZON10,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForTIMZONABBR10,
+			@ToDo @DataDef(length = 50) QCharacter cLVarForTIMZONFULL50,
+			@ToDo @DataDef(precision = 3) QDecimal cLVarForTIMOFFSET30,
+			@ToDo @DataDef(precision = 3) QDecimal cLVarForYEAROFS30,
+			@ToDo @DataDef(length = 15) QCharacter cLVarForACGCDE15,
+			@ToDo @DataDef(length = 8) QCharacter cLVarForSWS8,
+			@ToDo @DataDef(precision = 2) QDecimal cLVarForRUNPTY20,
+			@ToDo @DataDef(precision = 7) QDecimal cLVarForTIMESLICE70,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForPURGE10,
+			@ToDo @DataDef(precision = 7) QDecimal cLVarForDFTWAIT70,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForSBMMSGQ10,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForSBMMSGQLIB10,
+			@ToDo @DataDef(length = 5) QCharacter cLVarForDDMCNV5,
+			@ToDo @DataDef(length = 10) QCharacter cLVarForTSEPOOL10,
+			@ToDo @DataDef(length = 20) QCharacter cLVarForTHDRSCAFN20,
 			@ToDo @DataDef(length = 10) QCharacter cLVarForRSCAFNGRP10) {
 
+		
+		fillLibraries(userLibraries);
+		fillLibraries(sysLibraries);
+	}
+
+	private void fillLibraries(QCharacter userLibraries) {
 		userLibraries.clear();
 		int length = 0;
 		for (String library : job.getLibraries()) {
-			
 			if (userLibraries.isEmpty())
 				userLibraries.cat(library);
 			else
 				userLibraries.cat(library, 10 - length);
-			
+
 			length = library.length();
 		}
-
 	}
 }
