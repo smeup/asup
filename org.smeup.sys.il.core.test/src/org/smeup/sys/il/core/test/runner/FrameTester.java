8b0947cdd74d54cc771a1f35f832f9c84db1b581
darfores <darfores@ITERBNOT001.smeup.com>
2015-10-19 16:20:23 +0200

    Modulo dk.test: conversioni test esistenti

--- org.smeup.sys.il.core.test/src/org/smeup/sys/il/core/test/runner/FrameTester.java
+++ org.smeup.sys.il.core.test/src/org/smeup/sys/il/core/test/runner/FrameTester.java
@@ -18,7 +18,7 @@ import org.smeup.sys.dk.test.annotation.Test;
 import org.smeup.sys.dk.test.annotation.TestStarted;
 import org.smeup.sys.il.core.meta.QFrameManager;
 
-@Test(category = "il.core", object = "Frame")
+@Test(category = "IL.CORE", object = "Frame")
 public class FrameTester {
 
 	@Inject
