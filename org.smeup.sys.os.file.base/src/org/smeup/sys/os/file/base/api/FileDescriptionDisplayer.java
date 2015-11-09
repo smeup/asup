1464d1907f6c66bc5b147cc5afe92b0f71ea4669
Franco Lombardo <f.lombardo69@gmail.com>
2015-11-09 16:41:57 +0100

    DSPFD - Step 1

--- org.smeup.sys.os.file.base/src/org/smeup/sys/os/file/base/api/FileDescriptionDisplayer.java
+++ org.smeup.sys.os.file.base/src/org/smeup/sys/os/file/base/api/FileDescriptionDisplayer.java
@@ -4,6 +4,8 @@ import javax.inject.Inject;
 
 import org.smeup.sys.dk.core.annotation.Supported;
 import org.smeup.sys.dk.core.annotation.ToDo;
+import org.smeup.sys.dk.core.annotation.Unsupported;
+import org.smeup.sys.il.core.java.QStrings;
 import org.smeup.sys.il.core.out.QOutputManager;
 import org.smeup.sys.il.data.QCharacter;
 import org.smeup.sys.il.data.QDataStructWrapper;
@@ -16,13 +18,18 @@ import org.smeup.sys.il.data.annotation.Special;
 import org.smeup.sys.il.memo.QResourceManager;
 import org.smeup.sys.os.core.QExceptionManager;
 import org.smeup.sys.os.core.jobs.QJob;
+import org.smeup.sys.os.file.QDisplayFile;
 import org.smeup.sys.os.file.QFile;
+import org.smeup.sys.os.file.QLogicalFile;
+import org.smeup.sys.os.file.QPhysicalFile;
+import org.smeup.sys.os.file.QPrinterFile;
 import org.smeup.sys.os.file.base.api.FileFinder.FILE;
 
 @Program(name = "QWHDSPFD")
 public @Supported class FileDescriptionDisplayer {
 	public static enum QCPFMSG {
-		CPF3012     //File &1 non trovato nella libreria &2.
+		CPF3012,     //File &1 non trovato nella libreria &2.
+		CPF3020     //Nessun file di &1 in &2 ha il FILEATR specificato. 
 	}
 
 	@Inject
@@ -33,17 +40,19 @@ public @Supported class FileDescriptionDisplayer {
 	private QJob job;
 	@Inject
 	private QExceptionManager exceptionManager;
+	@Inject
+	private QStrings stringUtils;
 
 	
 	
 	public @Main void main(
 			@Supported @DataDef(qualified = true) FILE file,
-			@ToDo @DataDef(dimension = 10, length = 8) QEnum<TYPEOFINFORMATIONEnum, QScroller<QCharacter>> typeOfInformation,
-			@ToDo @DataDef(length = 1) QEnum<OUTPUTEnum, QCharacter> output,
-			@ToDo @DataDef(dimension = 12, length = 5) QEnum<FILEATTRIBUTESEnum, QScroller<QCharacter>> fileAttributes,
-			@ToDo @DataDef(qualified = true) FILETORECEIVEOUTPUT fileToReceiveOutput,
-			@ToDo OUTPUTMEMBEROPTIONS outputMemberOptions,
-			@ToDo @DataDef(length = 1) QEnum<SYSTEMEnum, QCharacter> system) {
+			@Supported @DataDef(dimension = 10, length = 8) QEnum<TYPEOFINFORMATIONEnum, QScroller<QCharacter>> typeOfInformation,
+			@Supported @DataDef(length = 1) QEnum<OUTPUTEnum, QCharacter> output,
+			@Supported @DataDef(dimension = 12, length = 5) QScroller<QCharacter> fileAttributes,
+			@Supported @DataDef(qualified = true) FILETORECEIVEOUTPUT fileToReceiveOutput,
+			@Unsupported OUTPUTMEMBEROPTIONS outputMemberOptions,
+			@Unsupported @DataDef(length = 1) QEnum<SYSTEMEnum, QCharacter> system) {
 
 		FileFinder fileFinder = new FileFinder(job, resourceManager);
 		QFile qFile = fileFinder.lookup(file);
@@ -51,6 +60,18 @@ public @Supported class FileDescriptionDisplayer {
 		if(qFile == null)
 			throw exceptionManager.prepareException(job, QCPFMSG.CPF3012, new String[] {file.nameGeneric.trimR(), file.library.asData().trimR()});		
 
+		checkType(fileAttributes, qFile);
+	}
+
+	private void checkType(QScroller<QCharacter> fileAttributes, QFile file) {
+		for (QCharacter attribute : fileAttributes) {
+			String attributeName = stringUtils.removeFirstChar(attribute.trimR());
+			if (!stringUtils.isEmptyTrim(attributeName) && FILEATTRIBUTESEnum.valueOf(attributeName).includes(file)) {
+				return;
+			}
+		}
+		
+		throw exceptionManager.prepareException(job, QCPFMSG.CPF3020, new String[] {file.getName(), file.getLibrary()});				
 	}
 
 	public static enum TYPEOFINFORMATIONEnum {
@@ -67,7 +88,41 @@ public @Supported class FileDescriptionDisplayer {
 	}
 
 	public static enum FILEATTRIBUTESEnum {
-		ALL, DSPF, PRTF, DKTF, TAPF, CMNF, BSCF, MXDF, PF, LF, SAVF, DDMF, ICFF
+		ALL {
+			@Override
+			public boolean includes(QFile arg0) {
+				return true;
+			}
+		}, 
+		DSPF {
+			@Override
+			public boolean includes(QFile file) {
+				return file instanceof QDisplayFile;
+			}
+		}, 
+		PRTF {
+			@Override
+			public boolean includes(QFile file) {
+				return file instanceof QPrinterFile;
+			}
+		}, 
+		PF  {
+			@Override
+			public boolean includes(QFile file) {
+				return file instanceof QPhysicalFile;
+			}
+		},	
+		LF {
+			@Override
+			public boolean includes(QFile file) {
+				return file instanceof QLogicalFile;
+			}
+		},
+		DKTF, TAPF,	CMNF, BSCF, MXDF,SAVF, DDMF, ICFF; 
+
+		public boolean includes(QFile file) {
+			throw new RuntimeException("Unsupported type " + this.name());
+		};
 	}
 
 	public static class FILETORECEIVEOUTPUT extends QDataStructWrapper {
@@ -84,8 +139,10 @@ public @Supported class FileDescriptionDisplayer {
 
 	public static class OUTPUTMEMBEROPTIONS extends QDataStructWrapper {
 		private static final long serialVersionUID = 1L;
+		
 		@DataDef(length = 10, value = "*FIRST")
 		public QEnum<MEMBERTORECEIVEOUTPUTEnum, QCharacter> memberToReceiveOutput;
+		
 		@DataDef(length = 1, value = "*REPLACE")
 		public QEnum<REPLACEORADDRECORDSEnum, QCharacter> replaceOrAddRecords;
 
@@ -95,15 +152,18 @@ public @Supported class FileDescriptionDisplayer {
 
 		public static enum REPLACEORADDRECORDSEnum {
 			@Special(value = "R")
-			REPLACE, @Special(value = "A")
+			REPLACE, 
+			@Special(value = "A")
 			ADD
 		}
 	}
 
 	public static enum SYSTEMEnum {
 		@Special(value = "L")
-		LCL, @Special(value = "R")
-		RMT, @Special(value = "A")
+		LCL,
+		@Special(value = "R")
+		RMT, 
+		@Special(value = "A")
 		ALL
 	}
 }
