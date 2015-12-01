5990bdcafc98251747c6362712450aef35870acd
Mattia Rocchi <mattiarocchi@gmail.com>
2015-12-01 10:33:46 +0100

    ESAM exceptions

---
+++ org.smeup.sys.il.esam/src/org/smeup/sys/il/esam/IntegratedLanguageEsamException.java
@@ -0,0 +1,24 @@
+/**
+ *  Copyright (c) 2012, 2015 Sme.UP and others. *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.smeup.sys.il.esam;
+
+public class IntegratedLanguageEsamException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public IntegratedLanguageEsamException(Throwable cause) {
+		this(cause.getMessage(), cause.getCause());
+	}
+
+	public IntegratedLanguageEsamException(String message) {
+		this(message, null);
+	}
+
+	public IntegratedLanguageEsamException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
