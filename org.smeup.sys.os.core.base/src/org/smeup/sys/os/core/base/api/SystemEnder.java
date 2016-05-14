770d5a7baee716035c8d2f815dc1412528528207
darfores <darfores@ITERBNOT001.smeup.com>
2016-05-14 10:47:57 +0200

    Allineato PWRDWNSYS a comando AS400

--- org.smeup.sys.os.core.base/src/org/smeup/sys/os/core/base/api/SystemEnder.java
+++ org.smeup.sys.os.core.base/src/org/smeup/sys/os/core/base/api/SystemEnder.java
@@ -14,18 +14,23 @@ package org.smeup.sys.os.core.base.api;
 import javax.inject.Inject;
 
 import org.smeup.sys.dk.core.annotation.Supported;
+import org.smeup.sys.dk.core.annotation.ToDo;
+import org.smeup.sys.il.data.QBinary;
 import org.smeup.sys.il.data.QCharacter;
+import org.smeup.sys.il.data.QDataStructWrapper;
 import org.smeup.sys.il.data.QEnum;
+import org.smeup.sys.il.data.QScroller;
 import org.smeup.sys.il.data.annotation.DataDef;
 import org.smeup.sys.il.data.annotation.Main;
 import org.smeup.sys.il.data.annotation.Program;
 import org.smeup.sys.il.data.annotation.Special;
+import org.smeup.sys.il.data.def.BinaryType;
 import org.smeup.sys.os.core.QSystemManager;
 import org.smeup.sys.os.core.jobs.QJob;
 import org.smeup.sys.rt.core.QApplicationManager;
 import org.smeup.sys.rt.repo.QRepositoryManager;
 
-@Program(name = "QWCCSDSC") 
+@Program(name = "QWCCSDSC")
 public class SystemEnder {
 
 	@Inject
@@ -36,12 +41,19 @@ public class SystemEnder {
 	private QSystemManager systemManager;
 	@Inject
 	private QJob job;
-	
+
 	@Main
-	public void main(@Supported @DataDef(length = 4) QEnum<RESTARTEnum, QCharacter> restart, @Supported @DataDef(length = 4) QEnum<UPGRADEEnum, QCharacter> upgrade) {
-		
+	public void main(@ToDo @DataDef(length = 1) QEnum<HOWTOENDEnum, QCharacter> howToEnd,
+			@Supported @DataDef(binaryType = BinaryType.INTEGER) QEnum<CONTROLLEDENDDELAYTIMEEnum, QBinary> controlledEndDelayTime,
+			@Supported @DataDef(length = 4) QEnum<UPGRADEEnum, QCharacter> upgrade,
+			@Supported RESTARTOPTIONS restartOptions, @DataDef(length = 1) QEnum<IPLSOURCEEnum, QCharacter> iPLSource,
+			@DataDef(length = 10) QCharacter imageCatalog,
+			@DataDef(dimension = 3, length = 10) QScroller<QEnum<ENDSUBSYSTEMOPTIONEnum, QCharacter>> endSubsystemOption,
+			@DataDef(length = 1) QEnum<TIMEOUTOPTIONEnum, QCharacter> timeoutOption,
+			@DataDef(length = 10) QEnum<CONFIRMEnum, QCharacter> confirm) {
+
 		systemManager.stop();
-		
+
 		switch (upgrade.asEnum()) {
 		case NO:
 			break;
@@ -49,9 +61,9 @@ public class SystemEnder {
 			repositoryManager.updateAll(job);
 			break;
 		}
-		
-		switch (restart.asEnum()) {
-		case NO:			
+
+		switch (restartOptions.restartAfterPowerDown.asEnum()) {
+		case NO:
 			applicationManager.stop();
 			break;
 		case YES:
@@ -60,15 +72,47 @@ public class SystemEnder {
 		}
 	}
 
-	public static enum RESTARTEnum {
-		@Special("*NO")
-		NO, @Special("*YES")
-		YES
+	public static enum HOWTOENDEnum {
+		@Special(value = "C") CNTRLD, @Special(value = "I") IMMED
+	}
+
+	public static enum CONTROLLEDENDDELAYTIMEEnum {
+		@Special(value = "999999") NOLIMIT, OTHER
 	}
 
 	public static enum UPGRADEEnum {
-		@Special("*NO")
-		NO, @Special("*YES")
-		YES
+		NO, YES
+	}
+
+	public static class RESTARTOPTIONS extends QDataStructWrapper {
+		private static final long serialVersionUID = 1L;
+		@DataDef(length = 1, value = "*NO")
+		public QEnum<RESTARTAFTERPOWERDOWNEnum, QCharacter> restartAfterPowerDown;
+		@DataDef(length = 1, value = "*IPLA")
+		public QEnum<RESTARTTYPEEnum, QCharacter> restartType;
+
+		public static enum RESTARTAFTERPOWERDOWNEnum {
+			@Special(value = "N") NO, @Special(value = "Y") YES
+		}
+
+		public static enum RESTARTTYPEEnum {
+			@Special(value = "I") IPLA, @Special(value = "S") SYS, @Special(value = "F") FULL
+		}
+	}
+
+	public static enum IPLSOURCEEnum {
+		@Special(value = "P") PANEL, @Special(value = "A") A, @Special(value = "B") B, @Special(value = "D") D, @Special(value = "I") IMGCLG
+	}
+
+	public static enum ENDSUBSYSTEMOPTIONEnum {
+		DFT, NOJOBLOG, CHGPTY, CHGTSL
+	}
+
+	public static enum TIMEOUTOPTIONEnum {
+		@Special(value = "C") CONTINUE, @Special(value = "M") MSD, @Special(value = "S") SYSREFCDE
+	}
+
+	public static enum CONFIRMEnum {
+		ENVVAR, INTERACT, YES, NO
 	}
 }
