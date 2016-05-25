daf7d910bd6c671eb23b5918210dd9c776b2bdad
giugianc73 <giuliano.giancristofaro@gmail.com>
2016-05-25 18:23:09 +0200

    Api Retrieve Call Stack (QWVRCSTK) - (TODO)

---
+++ org.smeup.sys.os.pgm.base/src/org/smeup/sys/os/pgm/base/api/CallStackRetriever.java
@@ -0,0 +1,13 @@
+package org.smeup.sys.os.pgm.base.api;
+
+import org.smeup.sys.il.data.annotation.Main;
+import org.smeup.sys.il.data.annotation.Program;
+
+@Program(name = "QWVRCSTK")
+public class CallStackRetriever {
+
+	@Main
+	public void main() {
+	}
+
+}
