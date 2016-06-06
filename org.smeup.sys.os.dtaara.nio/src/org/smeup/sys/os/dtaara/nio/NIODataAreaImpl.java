9cf42d57f65de6a8529ed42b3cc04ab7205af6e5
Mattia Rocchi <mattiarocchi@gmail.com>
2016-06-06 12:54:09 +0200

    Ottimizzazione dataArea

--- org.smeup.sys.os.dtaara.nio/src/org/smeup/sys/os/dtaara/nio/NIODataAreaImpl.java
+++ org.smeup.sys.os.dtaara.nio/src/org/smeup/sys/os/dtaara/nio/NIODataAreaImpl.java
@@ -29,7 +29,7 @@ public class NIODataAreaImpl<D extends QBufferedElement> extends NIOBufferedElem
 
 	private String externalName;
 	private org.smeup.sys.os.dtaara.QDataArea currentDataArea;
-	
+
 	protected NIODataAreaImpl(QDataContext dataContext, D delegate, String externalName) {
 		super(dataContext, delegate);
 		this.externalName = externalName;
@@ -40,20 +40,23 @@ public class NIODataAreaImpl<D extends QBufferedElement> extends NIOBufferedElem
 	public D get() {
 		return (D) getDelegate();
 	}
-	
+
 	@Override
 	public void in() {
 
+		if(currentDataArea != null) 
+			return;
+		
 		if (externalName.equalsIgnoreCase("*LDA")) {
 			QDataAreaManager dataAreaManager = getDataContext().getContext().get(QDataAreaManager.class);
 			currentDataArea = dataAreaManager.getLocalDataArea(getDataContext().getContext());
-		}
-		else {
+		} else {
 			QResourceManager resourceManager = getDataContext().getContext().get(QResourceManager.class);
 			QResourceReader<org.smeup.sys.os.dtaara.QDataArea> dataAreaReader = resourceManager.getResourceReader(getDataContext(), org.smeup.sys.os.dtaara.QDataArea.class, Scope.LIBRARY_LIST);
 			currentDataArea = dataAreaReader.lookup(externalName);
-		}		
-		
+		}
+
+		// let throw null pointer
 		get().movel(currentDataArea.getContent(), true);
 	}
 
@@ -61,29 +64,28 @@ public class NIODataAreaImpl<D extends QBufferedElement> extends NIOBufferedElem
 	public void in(QIndicator error) {
 		try {
 			in();
-			error.eval(false);			
+			error.eval(false);
 		} catch (RuntimeException e) {
 			error.eval(true);
 		}
 	}
-	
+
 	@Override
 	public void out() {
 
 		if (externalName.equalsIgnoreCase("*LDA")) {
 			// TODO
-			if(get() instanceof QString)
-				currentDataArea.setContent(((QString)get()).asString());
+			if (get() instanceof QString)
+				currentDataArea.setContent(((QString) get()).asString());
 			else
 				currentDataArea.setContent(new String(asBytes()));
-		}
-		else {
+		} else {
 			QResourceManager resourceManager = getDataContext().getContext().get(QResourceManager.class);
 			QResourceWriter<org.smeup.sys.os.dtaara.QDataArea> dataAreaWriter = resourceManager.getResourceWriter(getDataContext(), org.smeup.sys.os.dtaara.QDataArea.class, currentDataArea.getLibrary());
-			
+
 			org.smeup.sys.os.dtaara.QDataArea qDataArea = dataAreaWriter.lookup(externalName);
-			if(get() instanceof QString)
-				qDataArea.setContent(((QString)get()).asString());
+			if (get() instanceof QString)
+				qDataArea.setContent(((QString) get()).asString());
 			else
 				qDataArea.setContent(new String(asBytes()));
 			dataAreaWriter.save(qDataArea, true);
@@ -94,7 +96,7 @@ public class NIODataAreaImpl<D extends QBufferedElement> extends NIOBufferedElem
 	public void out(QIndicator error) {
 		try {
 			out();
-			error.eval(false);			
+			error.eval(false);
 		} catch (RuntimeException e) {
 			error.eval(true);
 		}
@@ -107,29 +109,29 @@ public class NIODataAreaImpl<D extends QBufferedElement> extends NIOBufferedElem
 
 	@Override
 	public void movea(QArray<? extends QString> value) {
-		
-		if(!(get() instanceof QString))
+
+		if (!(get() instanceof QString))
 			throw new UnsupportedOperationException();
-		
-		((QString)get()).movea(value);
+
+		((QString) get()).movea(value);
 	}
 
 	@Override
-	public void eval(QString value) {		
-		
-		if(!(get() instanceof QString))
+	public void eval(QString value) {
+
+		if (!(get() instanceof QString))
 			throw new UnsupportedOperationException();
-		
-		((QString)get()).eval(value);		
+
+		((QString) get()).eval(value);
 	}
 
 	@Override
 	public String asString() {
 
-		if(!(get() instanceof QString))
+		if (!(get() instanceof QString))
 			throw new UnsupportedOperationException();
-		
-		return ((QString)get()).asString();		
+
+		return ((QString) get()).asString();
 	}
 
 	@Override
