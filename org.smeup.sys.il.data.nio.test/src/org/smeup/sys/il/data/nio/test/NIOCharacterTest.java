0aed5683c2c338dc241db9340c213166df54d03f
Mattia Rocchi <mattiarocchi@gmail.com>
2016-06-13 10:56:51 +0200

    Primo rilascio NIOData test

---
+++ org.smeup.sys.il.data.nio.test/src/org/smeup/sys/il/data/nio/test/NIOCharacterTest.java
@@ -0,0 +1,35 @@
+/**
+ *  Copyright (c) 2012, 2016 Sme.UP and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *   Gianluca Fioletti - Initial API and implementation
+ */
+package org.smeup.sys.il.data.nio.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.smeup.sys.il.data.QString;
+import org.smeup.sys.il.data.nio.NIOCharacterImpl;
+import org.smeup.sys.il.data.nio.NIODataContextImpl;
+
+
+public class NIOCharacterTest {
+
+    @SuppressWarnings({ "nls", "static-method" })
+    @Test
+    public void testStrings() {
+    	
+    	NIODataContextImpl dataContextImpl = new NIODataContextImpl(null, null, null);
+    	
+    	
+        final QString string = new NIOCharacterImpl(dataContextImpl, 10, true);
+        string.eval("pippo");
+
+        Assert.assertTrue(string.eq("pippo"));
+    }
+}
---
+++ org.smeup.sys.il.data.nio.test/src/org/smeup/sys/il/data/nio/test/NIODecimalPackedTest.java
@@ -0,0 +1,36 @@
+/**
+ *  Copyright (c) 2012, 2016 Sme.UP and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *   Gianluca Fioletti - Initial API and implementation
+ */
+package org.smeup.sys.il.data.nio.test;
+
+import java.math.BigDecimal;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.smeup.sys.il.data.QDecimal;
+import org.smeup.sys.il.data.nio.NIODataContextImpl;
+import org.smeup.sys.il.data.nio.NIODecimalZonedImpl;
+
+
+public class NIODecimalPackedTest {
+
+    @SuppressWarnings({ "nls", "static-method" })
+    @Test
+    public void testEquals() {
+
+    	NIODataContextImpl dataContextImpl = new NIODataContextImpl(null, null, null);
+    	
+        final QDecimal v1 = new NIODecimalZonedImpl(dataContextImpl, 10, 2, true);
+        v1.eval(2.5);
+        Assert.assertTrue(v1.eq(BigDecimal.valueOf(2.5)));
+
+    }
+}
