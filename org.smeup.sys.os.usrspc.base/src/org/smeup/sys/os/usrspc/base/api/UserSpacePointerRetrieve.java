94bbd98356e6aa80d66b37d8f1b43210509e6851
giugianc73 <giuliano.giancristofaro@gmail.com>
2017-02-22 21:15:17 +0100

    Implementazione API UserSpacePointerRetrieve

--- org.smeup.sys.os.usrspc.base/src/org/smeup/sys/os/usrspc/base/api/UserSpacePointerRetrieve.java
+++ org.smeup.sys.os.usrspc.base/src/org/smeup/sys/os/usrspc/base/api/UserSpacePointerRetrieve.java
@@ -11,25 +11,51 @@
  */
 package org.smeup.sys.os.usrspc.base.api;
 
+import javax.inject.Inject;
+
 import org.smeup.sys.il.data.QBinary;
 import org.smeup.sys.il.data.QCharacter;
+import org.smeup.sys.il.data.QDataContext;
 import org.smeup.sys.il.data.QDataStructWrapper;
 import org.smeup.sys.il.data.QPointer;
+import org.smeup.sys.il.data.QString;
 import org.smeup.sys.il.data.annotation.DataDef;
 import org.smeup.sys.il.data.annotation.Main;
 import org.smeup.sys.il.data.annotation.Program;
 import org.smeup.sys.il.data.def.BinaryType;
+import org.smeup.sys.il.memo.QResourceManager;
+import org.smeup.sys.il.memo.QResourceReader;
+import org.smeup.sys.os.core.jobs.QJob;
+import org.smeup.sys.os.usrspc.QUserSpace;
 
 @Program(name = "QUSPTRUS")
 public class UserSpacePointerRetrieve {
 
-	@Main
-	public void main(@DataDef(qualified = true) UserSpaceRef userSpaceRef, 
-					 QPointer retunrPointer, 
-					 ErrorCode errorCode) {
-		"".toCharArray();
-	}
+	@Inject
+	private QResourceManager resourceManager;
+	@Inject
+	private QJob job;
+	@Inject
+	private QDataContext dataContext;
 
+	@Main
+	public void main(@DataDef(qualified = true) UserSpaceRef userSpaceRef, QPointer returnPointer, ErrorCode errorCode) {
+
+		QUserSpace userSpace = null;
+		QResourceReader<QUserSpace> userSpaceReader = resourceManager.getResourceReader(job, QUserSpace.class, userSpaceRef.library.asEnum(), userSpaceRef.library.asData().trimR());
+		userSpace = userSpaceReader.lookup(userSpaceRef.name.trimR());
+
+		if (userSpace == null) {
+			System.err.println("User space not found");
+			return;
+		}
+
+		byte[] bytes = userSpace.getContentArray();
+//		bytes = Arrays.copyOfRange(bytes, 0, 0);
+		QString receiverVariable = dataContext.getDataFactory().createCharacter(bytes.length, false, true);
+		receiverVariable.eval(new String(bytes, dataContext.getCharset()));
+		returnPointer.eval(receiverVariable.qAddr());
+	}
 
 	public static class ErrorCode extends QDataStructWrapper {
 		private static final long serialVersionUID = 1L;
