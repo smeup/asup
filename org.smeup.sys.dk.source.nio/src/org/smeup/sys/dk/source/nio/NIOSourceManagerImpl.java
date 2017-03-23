94a09e3dfd07978846a37710e335c590e0364802
giugianc73 <giuliano.giancristofaro@gmail.com>
2017-03-23 15:18:51 +0100

    Organize imports

--- org.smeup.sys.dk.source.nio/src/org/smeup/sys/dk/source/nio/NIOSourceManagerImpl.java
+++ org.smeup.sys.dk.source.nio/src/org/smeup/sys/dk/source/nio/NIOSourceManagerImpl.java
@@ -13,7 +13,6 @@ package org.smeup.sys.dk.source.nio;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
