f4e1c8f8499599887d5f8bfa2182487382f245e3
giugianc73 <giuliano.giancristofaro@gmail.com>
2017-04-10 12:57:07 +0200

    Aggiunta nuova parola riservata

--- org.smeup.sys.dk.compiler.rpj/src/org/smeup/sys/dk/compiler/rpj/RPJCompilationUnitImpl.java
+++ org.smeup.sys.dk.compiler.rpj/src/org/smeup/sys/dk/compiler/rpj/RPJCompilationUnitImpl.java
@@ -89,8 +89,8 @@ public class RPJCompilationUnitImpl extends CompilationUnitImpl {
 
 	private Map<String, QDataTerm<?>> cachedTerms = new HashMap<String, QDataTerm<?>>();
 	private Map<String, QPrototype> cachedPrototypes = new HashMap<String, QPrototype>();
-	private ArrayList<String> reservedKeywords = new ArrayList<String>(Arrays.asList("INT", "FOR", "CHAR", "IF", "BREAK", "TRUE", "FALSE", "DO", "FINAL", "DEFAULT", "ENUM", "CALL", "DOUBLE", "IMPORT", "NEW", "SWITCH", "VOID", "ELSE"));
-
+	private ArrayList<String> reservedKeywords = new ArrayList<String>(Arrays.asList("INT", "FOR", "CHAR", "IF", "BREAK", "TRUE", "FALSE", "DO", "FINAL", "DEFAULT", "ENUM", "CALL", "DOUBLE", "IMPORT", "NEW", "SWITCH", "VOID", "ELSE","QIN"));
+	// QIN --> C5005_IN
 	public RPJCompilationUnitImpl(QContext context, QNameable node, QCompilationUnit parentUnit, List<QCompilationUnit> childUnits, CaseSensitiveType caseSensitive) {
 
 		this.context = context;
