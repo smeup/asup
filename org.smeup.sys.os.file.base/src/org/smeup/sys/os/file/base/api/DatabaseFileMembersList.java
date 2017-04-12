7c870e67e1d58903ff7404801549aaef6c4c564e
giugianc73 <giuliano.giancristofaro@gmail.com>
2017-04-12 17:55:08 +0200

    Implementazione nuove Api

--- org.smeup.sys.os.file.base/src/org/smeup/sys/os/file/base/api/DatabaseFileMembersList.java
+++ org.smeup.sys.os.file.base/src/org/smeup/sys/os/file/base/api/DatabaseFileMembersList.java
@@ -11,30 +11,237 @@
  */
 package org.smeup.sys.os.file.base.api;
 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
 import org.smeup.sys.il.data.QBinary;
 import org.smeup.sys.il.data.QCharacter;
 import org.smeup.sys.il.data.QDataStructWrapper;
 import org.smeup.sys.il.data.QEnum;
 import org.smeup.sys.il.data.annotation.DataDef;
 import org.smeup.sys.il.data.annotation.Main;
+import org.smeup.sys.il.data.annotation.Overlay;
 import org.smeup.sys.il.data.annotation.Program;
+import org.smeup.sys.il.data.annotation.Special;
 import org.smeup.sys.il.data.def.BinaryType;
+import org.smeup.sys.il.memo.QResourceManager;
+import org.smeup.sys.il.memo.QResourceReader;
+import org.smeup.sys.il.memo.QResourceWriter;
+import org.smeup.sys.os.core.base.api.ErrorCodeRef;
+import org.smeup.sys.os.core.jobs.QJob;
+import org.smeup.sys.os.core.jobs.QJobManager;
+import org.smeup.sys.os.file.QFile;
+import org.smeup.sys.os.file.QFileMember;
+import org.smeup.sys.os.file.QFileMemberManager;
+import org.smeup.sys.os.file.QFileMembered;
+import org.smeup.sys.os.usrspc.QUserSpace;
+import org.smeup.sys.os.usrspc.base.api.UserSpaceRef;
 
 @Program(name = "QUSLMBR")
 public class DatabaseFileMembersList {
 
+	@Inject
+	private QJob job;
+	@Inject
+	private QResourceManager resourceManager;
+	@Inject
+	private QJobManager jobManager;
+	@Inject
+	private QFileMemberManager fileMemberManager;
+
+
+	
+	private USRSPC_HEADER_0100 userSpaceHeader;
+	private MBRL0100 mbrl0100;
+	private MBRL0200 mbrl0200;
+	
 	@Main 
-	public void main(@DataDef(qualified = true) OBJECT object, 
-							@DataDef(length = 8) QCharacter formatName,
-							@DataDef(qualified = true) OBJECT objectName,
+	public void main(@DataDef(qualified = true) UserSpaceRef userSpaceRef, 
+							@DataDef(length = 8) QEnum<FORMATEnum, QCharacter> formatName,
+							@DataDef(qualified = true) FileRef fileRef,
 							@DataDef(length = 10) QCharacter objectType,
 							@DataDef(length = 1) QCharacter overrideProcessing,
-							ErrorCode errorCode) {
-		// TODO
-		errorCode.£$01e2.eval(1);
+							ErrorCodeRef errorCode) {
+	
+		QResourceWriter<QUserSpace> userSpaceWriter = resourceManager.getResourceWriter(job, QUserSpace.class, userSpaceRef.library.asEnum(), userSpaceRef.library.asData().trimR());
+		QUserSpace userSpace = userSpaceWriter.lookup(userSpaceRef.name.trimR());
+		if (userSpace == null) {
+			errorCode.exceptionName.eval("002763");
+			errorCode.bytesAvailable.eval(1);
+			return;
+		}
+		userSpaceHeader.reset();
+		
+		userSpaceHeader.formatName.eval(formatName.asData());
+		userSpaceHeader.apiUsed.eval("QUSLMBR");
+		userSpaceHeader.dateTimeCreated.eval(Long.toString(jobManager.now(job).getTime()));
+		userSpaceHeader.informationStatus.eval(ACCURATEEnum.PARTIAL);
+
+		ArrayList<QFileMember> listObjects = getObjects(fileRef);
+		userSpaceHeader.entriesNumber.eval(listObjects.size());
+		userSpaceHeader.listSize.eval(userSpaceHeader.entriesNumber.qMult(mbrl0100.getSize()));
+
+		switch(formatName.asEnum()){
+		case MBRL0100:
+			userSpaceHeader.listOffset.eval(512);
+			userSpaceHeader.listSize.eval(userSpaceHeader.entriesNumber.qMult(mbrl0100.getSize()));
+			userSpaceHeader.entrySize.eval(mbrl0100.getSize());
+			break;
+		case MBRL0200:
+			userSpaceHeader.listOffset.eval(390);
+			userSpaceHeader.listSize.eval(userSpaceHeader.entriesNumber.qMult(mbrl0200.getSize()));
+			userSpaceHeader.entrySize.eval(mbrl0200.getSize());
+			break;
+		}
+		byte[] bytes = new byte[userSpaceHeader.listOffset.i() + userSpaceHeader.listSize.i()];
+		ByteBuffer buffer = ByteBuffer.wrap(bytes);
+		
+		buffer.put(userSpaceHeader.asBytes());
+		
+		buffer.position(userSpaceHeader.listOffset.i());
+		
+		for(QFileMember member: listObjects) {
+
+			mbrl0100.clear();
+			mbrl0200.clear();
+			
+			switch(formatName.asEnum()){
+			case MBRL0100:
+				setMbrl0100(member);
+				buffer.put(mbrl0100.asBytes());
+				break;
+			case MBRL0200:
+				setMbrl0100(member);
+				mbrl0200.member100Format.eval(mbrl0100);
+				mbrl0200.sourceType.eval(member.getType());;
+				mbrl0200.memberTextDescription.eval(member.getText());
+				buffer.put(mbrl0200.asBytes());
+				break;
+			}
+		}
+			
+		userSpace.setContentArray(bytes);
+		userSpaceWriter.save(userSpace, true);		
+	}
+	
+	private void setMbrl0100(QFileMember member) {
+		mbrl0100.memberNameUsed.eval(member.getName());
+	}
+	
+	private ArrayList<QFileMember> getObjects(FileRef fileRef) {
+		
+		ArrayList<QFileMember> fileMembers = new ArrayList<QFileMember>();
+
+		QResourceReader<QFile> fileReader = resourceManager.getResourceReader(job, QFile.class, fileRef.library.asEnum(), fileRef.library.asData().trimR()); 
+		for(QFile file: fileReader.find(fileRef.name.trimR())) {
+			if(!(file instanceof QFileMembered))
+				continue;
+			QFileMembered fileMembered = (QFileMembered)file;
+			for(QFileMember fileMember: fileMemberManager.list(job, fileMembered))
+				fileMembers.add(fileMember);
+		}		
+		
+		return fileMembers;
 	}
 
 
+	public static class MBRL0100 extends QDataStructWrapper {
+		private static final long serialVersionUID = 1L;
+		@DataDef(length = 10)
+		public QCharacter memberNameUsed;
+	}
+
+	public static class MBRL0200 extends QDataStructWrapper {
+		private static final long serialVersionUID = 1L;
+		@DataDef(length = 10)
+		public QCharacter member100Format;
+		@DataDef(length = 10)
+		public QCharacter sourceType;
+		@DataDef(length = 13)
+		public QCharacter creationDate;
+		@DataDef(length = 13)
+		public QCharacter lastChange;
+		@DataDef(length = 50)
+		public QCharacter memberTextDescription;
+		@DataDef(binaryType = BinaryType.INTEGER)
+		public QBinary memberTextDescriptionCCSID;
+	}
+	
+
+	public static class USRSPC_HEADER_0100 extends QDataStructWrapper {
+		private static final long serialVersionUID = 1L;
+		@DataDef(length = 64)
+		@Overlay(position = 1)
+		public QCharacter userArea;
+		@DataDef(binaryType = BinaryType.INTEGER, value = "128")
+		@Overlay(position = 65)
+		public QBinary userSpaceHeaderSize;
+		@DataDef(length = 4, value = "0100")
+		@Overlay(position = 69)
+		public QCharacter structureReleaseLevel;
+		@DataDef(length = 8)
+		@Overlay(position = 73)
+		public QCharacter formatName;
+		@DataDef(length = 10)
+		@Overlay(position = 81)
+		public QCharacter apiUsed;
+		@DataDef(length = 13)
+		@Overlay(position = 91)
+		public QCharacter dateTimeCreated;
+		@DataDef(length = 1)
+		@Overlay(position = 104)
+		public QEnum<ACCURATEEnum, QCharacter> informationStatus;
+		@DataDef(binaryType = BinaryType.INTEGER)
+		@Overlay(position = 105)
+		public QBinary userSpaceSizeUsed;
+		@DataDef(binaryType = BinaryType.INTEGER, value = "192")
+		@Overlay(position = 109)
+		public QBinary parameterOffset;
+		@DataDef(binaryType = BinaryType.INTEGER, value = "64")
+		@Overlay(position = 113)
+		public QBinary parameterSize;
+		@DataDef(binaryType = BinaryType.INTEGER, value = "256")
+		@Overlay(position = 117)
+		public QBinary headerOffset;
+		@DataDef(binaryType = BinaryType.INTEGER, value = "256")
+		@Overlay(position = 121)
+		public QBinary headerSize;
+		@DataDef(binaryType = BinaryType.INTEGER, value = "512")
+		@Overlay(position = 125)
+		public QBinary listOffset;
+		@DataDef(binaryType = BinaryType.INTEGER)
+		@Overlay(position = 129)
+		public QBinary listSize;
+		@DataDef(binaryType = BinaryType.INTEGER)
+		@Overlay(position = 133)
+		public QBinary entriesNumber;
+		@DataDef(binaryType = BinaryType.INTEGER)
+		@Overlay(position = 137)
+		public QBinary entrySize;
+
+		@DataDef(binaryType = BinaryType.INTEGER)
+		@Overlay(position = 141)
+		public QBinary entryCCSID;
+		@DataDef(length = 2)
+		@Overlay(position = 145)
+		public QCharacter countryID;
+		@DataDef(length = 3)
+		@Overlay(position = 147)
+		public QCharacter languageID;
+		@DataDef(length = 1)
+		@Overlay(position = 150)
+		public QCharacter listIndicator;
+		@DataDef(length = 42)
+		@Overlay(position = 151)
+		public QCharacter reserved01;
+	}	
+
+	public static enum ACCURATEEnum {
+		@Special(value = "C") COMPLETE, @Special(value = "I") INCOMPLETE, @Special(value = "P") PARTIAL;
+	}
+	
 	public static class OBJECT extends QDataStructWrapper {
 		private static final long serialVersionUID = 1L;
 		@DataDef(length = 10)
@@ -52,17 +259,7 @@ public class DatabaseFileMembersList {
 		}
 	}
 
-	public static class ErrorCode extends QDataStructWrapper {
-		private static final long serialVersionUID = 1L;
-		@DataDef(binaryType = BinaryType.INTEGER, value = "272")
-		public QBinary £$01e1;
-		@DataDef(binaryType = BinaryType.INTEGER)
-		public QBinary £$01e2;
-		@DataDef(length = 7)
-		public QCharacter £$01e3;
-		@DataDef(length = 1)
-		public QCharacter £$01e4;
-		@DataDef(length = 256)
-		public QCharacter £$01e5;
+	public static enum FORMATEnum {
+		@Special(value = "MBRL0100") MBRL0100, @Special(value = "MBRL0200") MBRL0200;
 	}
 }
--- org.smeup.sys.os.type.base/src/org/smeup/sys/os/type/base/api/ObjectsList.java
+++ org.smeup.sys.os.type.base/src/org/smeup/sys/os/type/base/api/ObjectsList.java
@@ -11,23 +11,211 @@
  */
 package org.smeup.sys.os.type.base.api;
 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.smeup.sys.il.core.QObject;
+import org.smeup.sys.il.core.QObjectIterator;
 import org.smeup.sys.il.data.QBinary;
 import org.smeup.sys.il.data.QCharacter;
 import org.smeup.sys.il.data.QDataStructWrapper;
 import org.smeup.sys.il.data.QEnum;
 import org.smeup.sys.il.data.annotation.DataDef;
 import org.smeup.sys.il.data.annotation.Main;
+import org.smeup.sys.il.data.annotation.Overlay;
 import org.smeup.sys.il.data.annotation.Program;
+import org.smeup.sys.il.data.annotation.Special;
 import org.smeup.sys.il.data.def.BinaryType;
+import org.smeup.sys.il.memo.QResourceManager;
+import org.smeup.sys.il.memo.QResourceReader;
+import org.smeup.sys.il.memo.QResourceWriter;
+import org.smeup.sys.il.memo.Scope;
+import org.smeup.sys.os.core.base.api.ErrorCodeRef;
+import org.smeup.sys.os.core.jobs.QJob;
+import org.smeup.sys.os.core.jobs.QJobLogManager;
+import org.smeup.sys.os.core.jobs.QJobManager;
+import org.smeup.sys.os.type.QType;
+import org.smeup.sys.os.type.QTypeRegistry;
+import org.smeup.sys.os.type.QTypedObject;
+import org.smeup.sys.os.type.base.api.ObjectsList.OBJECT.NameGenericEnum;
+import org.smeup.sys.os.usrspc.QUserSpace;
+import org.smeup.sys.os.usrspc.base.api.UserSpaceRef;
 
 @Program(name = "QUSLOBJ")
 public class ObjectsList {
 
+	@Inject
+	private QResourceManager resourceManager;
+	@Inject
+	private QJobManager jobManager;
+	@Inject
+	private QJob job;
+	@Inject
+	private QTypeRegistry typeRegistry;
+	@Inject
+	private QJobLogManager jobLogManager;
+	
+	private USRSPC_HEADER_0100 userSpaceHeader;
+	private OBJL0100 objl0100;
+	private OBJL0200 objl0200;
+	
 	@Main
-	public void main(@DataDef(qualified = true) OBJECT object, @DataDef(length = 8) QCharacter formatName, @DataDef(qualified = true) OBJECT objectName, @DataDef(length = 10) QCharacter objectType,
-			ErrorCode errorCode) {
-		// TODO
-		errorCode.£$01e2.eval(1);
+	public void main(@DataDef(qualified = true) UserSpaceRef userSpaceRef, 
+			@DataDef(length = 8) QEnum<FORMATEnum, QCharacter> formatName, 
+			@DataDef(qualified = true) OBJECT objectName, 
+			@DataDef(length = 10) QCharacter objectType,
+			ErrorCodeRef errorCode) {
+
+		QResourceWriter<QUserSpace> userSpaceWriter = resourceManager.getResourceWriter(job, QUserSpace.class, userSpaceRef.library.asEnum(), userSpaceRef.library.asData().trimR());
+		QUserSpace userSpace = userSpaceWriter.lookup(userSpaceRef.name.trimR());
+		if (userSpace == null) {
+			errorCode.exceptionName.eval("002763");
+			errorCode.bytesAvailable.eval(1);
+			return;
+		}
+		userSpaceHeader.reset();
+
+		userSpaceHeader.formatName.eval(formatName.asData());
+		userSpaceHeader.apiUsed.eval("QUSLOBJ");
+		userSpaceHeader.dateTimeCreated.eval(Long.toString(jobManager.now(job).getTime()));
+		userSpaceHeader.informationStatus.eval(ACCURATEEnum.PARTIAL);
+
+		ArrayList<QObject> listObjects = getObjects(objectName, objectType);
+		userSpaceHeader.entriesNumber.eval(listObjects.size());
+		userSpaceHeader.listSize.eval(userSpaceHeader.entriesNumber.qMult(objl0100.getSize()));
+
+		switch(formatName.asEnum()){
+		case OBJL0100:
+			userSpaceHeader.listOffset.eval(512);
+			userSpaceHeader.listSize.eval(userSpaceHeader.entriesNumber.qMult(objl0100.getSize()));
+			userSpaceHeader.entrySize.eval(objl0100.getSize());
+			break;
+		case OBJL0200:
+			userSpaceHeader.listOffset.eval(390);
+			userSpaceHeader.listSize.eval(userSpaceHeader.entriesNumber.qMult(objl0200.getSize()));
+			userSpaceHeader.entrySize.eval(objl0200.getSize());
+			break;
+		}
+		byte[] bytes = new byte[userSpaceHeader.listOffset.i() + userSpaceHeader.listSize.i()];
+		ByteBuffer buffer = ByteBuffer.wrap(bytes);
+		
+		buffer.put(userSpaceHeader.asBytes());
+		
+		buffer.position(userSpaceHeader.listOffset.i());
+		for(QObject objectChild: listObjects) {
+
+			objl0100.clear();
+			objl0200.clear();
+			
+			switch(formatName.asEnum()){
+			case OBJL0100:
+				setObjl0100(objectChild, objectType);
+				buffer.put(objl0100.asBytes());
+				break;
+			case OBJL0200:
+				setObjl0100(objectChild, objectType);
+				objl0200.objectl0100Format.eval(objl0100);
+				// TODO
+				buffer.put(objl0200.asBytes());
+				break;
+			}
+
+		}
+			
+		userSpace.setContentArray(bytes);
+		userSpaceWriter.save(userSpace, true);		
+	}
+
+	private void setObjl0100(QObject objectChild, QCharacter objectType) {
+
+		QTypedObject qTypedObject = (QTypedObject) objectChild;
+		
+		objl0100.objectNameUsed.eval(qTypedObject.getName());
+		objl0100.objectLibraryUsed.eval(qTypedObject.getLibrary());
+		objl0100.objectTypeUsed.eval(objectType);
+	}
+	
+	private ArrayList<QObject> getObjects(OBJECT objectName, QCharacter objectType) {
+		ArrayList<QObject> objects = new ArrayList<QObject>();
+
+		List<QType<?>> types = findTypes(objectType);
+		for (QType<?> type : types) {
+			QResourceReader<?> resourceReader = findReader(objectName, type);
+			if (type.getName().equals("*TYPE"))
+				continue;
+			try (QObjectIterator<?> objectIterator = findIterator(objectName.nameGeneric , resourceReader);) {
+				while (objectIterator.hasNext()) {
+					QObject qObject = null;
+					try {
+						qObject = objectIterator.next();
+
+						if (qObject instanceof QTypedObject) {
+							QTypedObject qTypedObject = (QTypedObject) qObject;
+							objects.add(qTypedObject);
+						}
+					} catch (Exception e) {
+						jobLogManager.error(job, qObject + " " + e.getMessage());
+					}
+				}
+			}
+		}
+		return objects;
+	}
+
+
+	private List<QType<?>> findTypes(QCharacter objectType) {
+		List<QType<?>> types = new ArrayList<QType<?>>();
+		if (objectType.trimR().equals("*ALL"))
+			types.addAll(typeRegistry.list());
+		else
+			types.add(typeRegistry.lookup(objectType.trimR()));
+		return types;
+	}
+	
+	private QResourceReader<?> findReader(OBJECT object, QType<?> type) {
+		QResourceReader<?> resourceReader = null;
+		switch (object.library.asEnum()) {
+		case ALL:
+			resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), Scope.ALL);
+			break;
+		case ALLUSR:
+			resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), Scope.ALL_USER);
+			break;
+		case CURLIB:
+			resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), Scope.CURRENT_LIBRARY);
+			break;
+		case LIBL:
+			resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), Scope.LIBRARY_LIST);
+			break;
+		case USRLIBL:
+			resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), Scope.USER_LIBRARY_LIST);
+			break;
+		case OTHER:
+			resourceReader = resourceManager.getResourceReader(job, type.getTypedClass(), object.library.asData().trimR());
+			break;
+		}
+		return resourceReader;
+	}
+	
+	@SuppressWarnings("resource")
+	private QObjectIterator<?> findIterator(QEnum<NameGenericEnum, QCharacter> nameGeneric, QResourceReader<?> resourceReader) {
+		QObjectIterator<?> result = null;
+		switch (nameGeneric.asEnum()) {
+		case ALL:
+			result = resourceReader.find(null);
+			break;
+		case OTHER:
+			result = resourceReader.find(nameGeneric.asData().trimR());
+			break;
+		}
+		return result;
+	}
+
+	public static enum FORMATEnum {
+		@Special(value = "OBJL0100") OBJL0100, @Special(value = "OBJL0200") OBJL0200;
 	}
 
 	public static class OBJECT extends QDataStructWrapper {
@@ -47,17 +235,101 @@ public class ObjectsList {
 		}
 	}
 
-	public static class ErrorCode extends QDataStructWrapper {
+	public static enum ACCURATEEnum {
+		@Special(value = "C") COMPLETE, @Special(value = "I") INCOMPLETE, @Special(value = "P") PARTIAL;
+	}
+	
+	public static class OBJL0100 extends QDataStructWrapper {
 		private static final long serialVersionUID = 1L;
-		@DataDef(binaryType = BinaryType.INTEGER, value = "272")
-		public QBinary £$01e1;
-		@DataDef(binaryType = BinaryType.INTEGER)
-		public QBinary £$01e2;
-		@DataDef(length = 7)
-		public QCharacter £$01e3;
+		@DataDef(length = 10)
+		public QCharacter objectNameUsed;
+		@DataDef(length = 10)
+		public QCharacter objectLibraryUsed;
+		@DataDef(length = 6)
+		public QCharacter objectTypeUsed;
+	}
+
+	public static class OBJL0200 extends QDataStructWrapper {
+		private static final long serialVersionUID = 1L;
+		@DataDef(length = 30)
+		public QCharacter objectl0100Format;
 		@DataDef(length = 1)
-		public QCharacter £$01e4;
-		@DataDef(length = 256)
-		public QCharacter £$01e5;
+		public QCharacter objectInformationStatus;
+		@DataDef(length = 10)
+		public QCharacter extendedObjectAttribute;
+		@DataDef(length = 50)
+		public QCharacter textDescription;
+		@DataDef(length = 10)
+		public QCharacter userDefinedAttribute;
+		@DataDef(length = 7)
+		public QCharacter reserved;
+	}
+
+	public static class USRSPC_HEADER_0100 extends QDataStructWrapper {
+		private static final long serialVersionUID = 1L;
+		@DataDef(length = 64)
+		@Overlay(position = 1)
+		public QCharacter userArea;
+		@DataDef(binaryType = BinaryType.INTEGER, value = "128")
+		@Overlay(position = 65)
+		public QBinary userSpaceHeaderSize;
+		@DataDef(length = 4, value = "0100")
+		@Overlay(position = 69)
+		public QCharacter structureReleaseLevel;
+		@DataDef(length = 8)
+		@Overlay(position = 73)
+		public QCharacter formatName;
+		@DataDef(length = 10)
+		@Overlay(position = 81)
+		public QCharacter apiUsed;
+		@DataDef(length = 13)
+		@Overlay(position = 91)
+		public QCharacter dateTimeCreated;
+		@DataDef(length = 1)
+		@Overlay(position = 104)
+		public QEnum<ACCURATEEnum, QCharacter> informationStatus;
+		@DataDef(binaryType = BinaryType.INTEGER)
+		@Overlay(position = 105)
+		public QBinary userSpaceSizeUsed;
+		@DataDef(binaryType = BinaryType.INTEGER, value = "192")
+		@Overlay(position = 109)
+		public QBinary parameterOffset;
+		@DataDef(binaryType = BinaryType.INTEGER, value = "64")
+		@Overlay(position = 113)
+		public QBinary parameterSize;
+		@DataDef(binaryType = BinaryType.INTEGER, value = "256")
+		@Overlay(position = 117)
+		public QBinary headerOffset;
+		@DataDef(binaryType = BinaryType.INTEGER, value = "256")
+		@Overlay(position = 121)
+		public QBinary headerSize;
+		@DataDef(binaryType = BinaryType.INTEGER, value = "512")
+		@Overlay(position = 125)
+		public QBinary listOffset;
+		@DataDef(binaryType = BinaryType.INTEGER)
+		@Overlay(position = 129)
+		public QBinary listSize;
+		@DataDef(binaryType = BinaryType.INTEGER)
+		@Overlay(position = 133)
+		public QBinary entriesNumber;
+		@DataDef(binaryType = BinaryType.INTEGER)
+		@Overlay(position = 137)
+		public QBinary entrySize;
+
+		@DataDef(binaryType = BinaryType.INTEGER)
+		@Overlay(position = 141)
+		public QBinary entryCCSID;
+		@DataDef(length = 2)
+		@Overlay(position = 145)
+		public QCharacter countryID;
+		@DataDef(length = 3)
+		@Overlay(position = 147)
+		public QCharacter languageID;
+		@DataDef(length = 1)
+		@Overlay(position = 150)
+		public QCharacter listIndicator;
+		@DataDef(length = 42)
+		@Overlay(position = 151)
+		public QCharacter reserved01;
 	}
 }
